8ef4b7871545c38d38c5f536f277c5dc9e5a349e
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2022-07-16 09:57:07 +0700

    uodate breeding

--- prototype_mobile_sultanfarm/lib/view/Breeding.dart
+++ prototype_mobile_sultanfarm/lib/view/Breeding.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Paket Breeding',
-      debugShowCheckedModeBanner: false,
-      theme:
-          ThemeData(primarySwatch: Colors.blue, platform: TargetPlatform.iOS),
-    );
-  }
-}
+import 'package:google_fonts/google_fonts.dart';
+import 'package:prototype_mobile_sultanfarm/view/Profil.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -18,26 +8,66 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Pilihan Paket Breeding")),
-      body: ListView.builder(
-        itemCount: _images.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text('Paket Breeding ' + _Judul[index].toString()),
-            leading: Hero(
-              tag: index,
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child:
-                    Image.asset(_images[index], width: 100, fit: BoxFit.cover),
-              ),
+      appBar: AppBar(
+        title: Text("Pilihan Paket Breeding"),
+        backgroundColor: Color(0xFFFFAF00),
+      ),
+      backgroundColor: Color(0xFFFFAF00),
+      body: Container(
+        child: Card(
+          child: Container(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: ListView.builder(
+              itemCount: _images.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: <Widget>[
+                    // Padding(
+                    //     padding: EdgeInsets.only(
+                    //         right: 10, left: 10, bottom: 0.1, top: 0.1)),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      shadowColor: Colors.black,
+                      color: Color(0xFFFFAF00),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      SecondPage(heroTag: index)));
+                        },
+                        child: Container(
+                          child: Column(children: <Widget>[
+                            Image.asset(
+                              _images[index],
+                              width: double.infinity,
+                              height: 150,
+                              fit: BoxFit.fill,
+                            ),
+                            Text(
+                              "Paket Breeding " + _Judul[index].toString(),
+                              style: GoogleFonts.getFont(
+                                'Poppins',
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ]),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    )
+                  ],
+                );
+              },
             ),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => SecondPage(heroTag: index)));
-            },
-          );
-        },
+          ),
+        ),
       ),
     );
   }
@@ -50,7 +80,10 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Paket Breeding")),
+      appBar: AppBar(
+        title: Text("Paket Breeding " + _Judul[heroTag].toString()),
+        backgroundColor: Color(0xFFFFAF00),
+      ),
       body: Column(
         children: [
           Expanded(
@@ -62,11 +95,8 @@ class SecondPage extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Text(
-              _Deskripsi.toString(),
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          )
+            child: Text("Paket " + _Judul[heroTag].toString()),
+          ),
         ],
       ),
     );
@@ -79,5 +109,4 @@ final List<String> _images = [
   'assets/images/slider2.jpg'
 ];
 final List<String> _Judul = ['A', 'B', 'C'];
-
-final List<String> _Deskripsi = ["hhac", 'hvusuvhs', 'jcaihuc'];
+final List<String> _Deskripsi = [];
