78cc325f5ba8e9f05d2b45a7d9d9f2ffc59beee7
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2022-07-17 22:24:57 +0700

    upload artikel dan pembaruan breeding

--- prototype_mobile_sultanfarm/lib/view/artikel-view/artikel-paketbreeding.dart
+++ prototype_mobile_sultanfarm/lib/view/artikel-view/artikel-paketbreeding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class artikelPaketBreeding extends StatefulWidget {
   const artikelPaketBreeding({Key? key}) : super(key: key);
@@ -10,6 +11,44 @@ class artikelPaketBreeding extends StatefulWidget {
 class _artikelPaketBreedingState extends State<artikelPaketBreeding> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Penjelasan Paket Breeding"),
+          backgroundColor: Color(0xFFFFAF00),
+        ),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            SizedBox(
+              height: 15,
+            ),
+            Card(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  "assets/images/artikel/breeding2.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              child: Center(
+                  child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Text(
+                    "Paket Breeding adalah paket domba yang ditawarkan oleh CV Sultan Farm kepada para calon pelanggan untuk mendapatkan domba berkualitas dengan harga yang terjangkau dan lebih hemat dari membeli domba per sautan. Keuntungan lain dari paket breeding adalah pembelian paket ini selain ditujukan untuk konsumsi, domba dari paket breeding yang dipilih dapat dirawat atau dikelola sampai beranak binak dan hasil atau keuntungan dari penjualan anak domba ini akan dibagi dua antara investor dan pengelola, untuk mengetahui lebih detail tentang pembagian ini silahkan baca artikel selanjutnya tentang Investasi Syariah.",
+                    style: GoogleFonts.getFont(
+                      "Poppins",
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              )),
+            )
+          ]),
+        ));
   }
 }
--- prototype_mobile_sultanfarm/lib/view/artikel-view/artikel-realtimemonitoring.dart
+++ prototype_mobile_sultanfarm/lib/view/artikel-view/artikel-realtimemonitoring.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class artikelRealtimeMonitoring extends StatefulWidget {
   const artikelRealtimeMonitoring({Key? key}) : super(key: key);
@@ -11,6 +12,44 @@ class artikelRealtimeMonitoring extends StatefulWidget {
 class _artikelRealtimeMonitoringState extends State<artikelRealtimeMonitoring> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Penjelasan Realtime Monitoring"),
+          backgroundColor: Color(0xFFFFAF00),
+        ),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            SizedBox(
+              height: 15,
+            ),
+            Card(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  "assets/images/artikel/cctv.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              child: Center(
+                  child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Text(
+                    "Monitoring adalah layanan yang ditawarkan oleh CV Sultan Farm. Fungsi utama dari penerapan teknologi ini adalah menjamin konidisi kandang agar tetap bersih dan sehat, serta mengawasi keadaan domba. Jika suatu waktu domba mengalami gejala penyakit, ada pencuri dan lain-lain sebagainya maka pengawas dapat memberitahu pengelola supaya segera bertindak.",
+                    style: GoogleFonts.getFont(
+                      "Poppins",
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              )),
+            )
+          ]),
+        ));
   }
 }
